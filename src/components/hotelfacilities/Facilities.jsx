c93ac4384cf2faa65300a3c3dca0fa7ab1358e07
sabita <sabitakhadka091@gmail.com>
2025-03-19 14:34:35 +0545

    feat: created contact page with all the features which is need

--- src/components/hotelfacilities/Facilities.jsx
+++ src/components/hotelfacilities/Facilities.jsx
@@ -1,58 +1,83 @@
-import { Group, Text, Box, Image, Flex, Input, Center } from "@mantine/core";
+import { Text, Box, Flex, Card, Button } from "@mantine/core";
+import {
+  IconAntennaBars1,
+  IconBarbell,
+  IconBolt,
+  IconDevicesPc,
+  IconParkingCircle,
+  IconWifi,
+} from "@tabler/icons-react";
 
 const Facilities = () => {
+  const cardData = [
+    {
+      icon: <IconDevicesPc size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "Private Workspace",
+    },
+    {
+      icon: <IconParkingCircle size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "Parking Area",
+    },
+    {
+      icon: <IconWifi size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "High-Speed-Wifi",
+    },
+    {
+      icon: <IconBolt size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "Power Backup",
+    },
+    {
+      icon: <IconBarbell size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "Gym and Fitness",
+    },
+    {
+      icon: <IconAntennaBars1 size={40} stroke={1.5} color="#0A7D8F" />,
+      title: "Other Services",
+    },
+  ];
+
   return (
-    <Box pb={"2rem"}>
+    <Flex m={"0.5rem 0rem 0rem 2.6rem"} p={"2.2rem"} gap={"6rem"}>
+      <Box size="1.9rem" mb={10} w={"43rem"} p={"0.1rem"}>
+        <Text size="2.9rem" weight={900} mb={10}>
+          We do our Best facilities provide you
+        </Text>
+        <Text c="dimmed" mt={20} >
+          Enjoy top-notch amenities that make your stay comfortable and
+          convenient.
+        </Text>
+        <Button variant="filled" radius="xl" color="#008080" mr={"4rem"} mt={"3rem"}>
+          Contact Now
+        </Button>
+      </Box>
+
       <Flex
-        m={"0.5rem 0rem 0rem 5rem"}
-        bg={" #ebfcfc"}
-        // bg={"#f5f5f5"}
-        border={"1px solid #dedede"}
         justify={"center"}
         align={"center"}
-        w={"70rem"}
-        style={{
-          boxShadow: "5px 5px 5px 4px #f5f5f5",
-          borderStartStartRadius: "1rem",
-          borderStartEndRadius: "1rem",
-        }}
-        gap={"16rem"}
+        wrap="wrap"
+        mr={"2.9rem"}
+        gap={"2rem"}
       >
-        <Box pl={`3rem`}>
-          <Text size="2.9rem" weight={900} mb={10}>
-            Find Your Perfect Room, Anytime, Anywhere
-          </Text>
-          <Text c="dimmed" mb={20}>
-            Discover verified listings, flexible leases, and secure payments.
-            Renting made easy for everyone!
-          </Text>
-          <Group
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            w={"11rem"}
+            h={"10rem"}
             style={{
-              cursor: "pointer",
+              border: "0.9px solid #dedede",
+              padding: "2.8rem 0rem 2rem 0rem",
+              boxShadow: "5px 5px 5px 5px #f5f5f5",
             }}
           >
-            <Text weight={600}>Watch video</Text>
-          </Group>
-        </Box>
-        <Image
-          src="https://img.staticmb.com/mbcontent/images/crop/uploads/2024/6/Cyan-Colour-in-Bedroom_0_1200.jpg"
-          h={450}
-          alt="House"
-          style={{
-            borderStartEndRadius: "1rem",
-          }}
-        />
+            <Card.Section align="center">{card.icon}</Card.Section>
+
+            <Text fw={500} size="1rem" mt="md" align="center">
+              {card.title}
+            </Text>
+          </Card>
+        ))}
       </Flex>
-      <Center mt={-35}>
-        <Input
-          placeholder="Search rooms by location, budget, amenities, and more..."
-          // m={20}
-          radius="10rem"
-          size="md"
-          w={"54rem"}
-        />
-      </Center>
-    </Box>
+    </Flex>
   );
 };
 
