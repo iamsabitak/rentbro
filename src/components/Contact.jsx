93fdf9f2c90849c424d5611fc3cbef05e0844150
sabita <sabitakhadka091@gmail.com>
2025-03-19 18:17:21 +0545

    chores: make a basic sturcture

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,16 @@
+import { Box, Text } from "@mantine/core";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Box m={"2.9rem 2rem 0rem 5rem"}>
+      <Text size="2.9rem" weight={900} mb={10}>
+        Contact Us
+      </Text>
+      <Text c="dimmed" mt={20}>
+        We are here to help you with any queries you have.
+      </Text>
+    </Box>
+  );
 };
 
 export default Contact;
